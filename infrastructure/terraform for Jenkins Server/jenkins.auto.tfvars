b15080a54b65e015b800ad46a51bf2514dc663d5
Vy <eemeome@gmail.com>
2022-10-22 23:51:13 +0000

    added jenkins server terraform files and updated petclinicserver-userdata.sh

---
+++ infrastructure/terraform for Jenkins Server/jenkins.auto.tfvars
@@ -0,0 +1,8 @@
+mykey = "" # enter name of your keypair
+ami = "ami-09d3b3274b6c5d4aa"
+region = "us-east-1"
+instance_type = "t3a.medium"
+jenkins_server_secgr = "petclinic-jenkins-server-secgr"
+jenkins-server-tag = "Jenkins Server of Petclinic"
+jenkins-profile = "petclinic-jenkins-server-profile"
+jenkins-role = "petclinic-jenkins-server-role"
--- infrastructure/terraform for dev server/petclinicserver-userdata.sh
+++ infrastructure/terraform for dev server/petclinicserver-userdata.sh
@@ -5,7 +5,7 @@ amazon-linux-extras install docker -y
 systemctl start docker
 systemctl enable docker
 usermod -a -G docker ec2-user
-curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" \
+curl -L "https://github.com/docker/compose/releases/download/2.12.2/docker-compose-$(uname -s)-$(uname -m)" \
 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 yum install git -y
